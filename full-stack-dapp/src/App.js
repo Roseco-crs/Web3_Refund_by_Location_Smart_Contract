02e36dd77ad64c9f602c868c8f22af69c65a7e21
Roseco-crs <rodolphe.segbedji.aims.ac.rw>
2024-02-08 07:32:40 +0100

    created an ABI, configured hardhat.config.js, wrote a frontend, compiled the smart contract, deployed the smart contract

---
+++ full-stack-dapp/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import {ethers} from 'ethers';
+import Lock from "./artifacts/contracts/Lock.sol/Lock.json";
+import logo from './logo.svg';
+import './App.css';
+
+//import ABI code to interact with smart contract
+const lockAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+
+function App() {
+  // helper functions
+  // request access to the user's MetaMask Wallet account
+  async function requestAccount() {
+    await window.ethereum.request({method: "eth_requestAccounts"})
+  }
+  // Fetches the current value store in Lock
+  async function fetchLock() {
+    // If Metamask exist
+    if (typeof window.ethereum != "undefined") {
+      //const provider = new ethers.providers.Web3Provider(window.ethereum);
+      //const contract = new ethers.Contract(lockAddress, Lock.abi, provider)
+    }
+
+  }
+
+
+  return (
+    <div className="App">
+        <div className='App-header'>
+          {/* BUTTONS - Fetch and Set */}
+          <div className='Custom-button'>
+            <button> Fetch Lock </button>
+            <button> Set Lock </button>
+          </div>
+          {/* INPUT TEXT - String */}
+          <input
+              placeholder='Set Lock Message' 
+          />
+
+      </div>
+      
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
